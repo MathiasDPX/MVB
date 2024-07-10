729c001610c71c3bd62996a9f4edfc2d72db25dd
MathiasDPX <mathias@dupeux.net>
2024-07-10 20:22:18 +0200

    Initial commit

---
+++ src/main/java/gg/gyro/mab/MAB.java
@@ -0,0 +1,17 @@
+package gg.gyro.mab;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MAB extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
