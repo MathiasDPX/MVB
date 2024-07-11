55f5e3e1969b1b13ec8a3f814c7dfd98015f56de
MathiasDPX <mathias@dupeux.net>
2024-07-11 20:21:38 +0200

    Add database and config

--- src/main/java/gg/gyro/mab/MAB.java
+++ src/main/java/gg/gyro/mab/MAB.java
@@ -2,15 +2,23 @@ package gg.gyro.mab;
 
 import gg.gyro.mab.commands.*;
 import gg.gyro.mab.listeners.*;
+import gg.gyro.mab.utils.Database;
 
 import revxrsal.commands.bukkit.BukkitCommandHandler;
 import org.bukkit.plugin.java.JavaPlugin;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public final class MAB extends JavaPlugin {
+    Database db = new Database(this);
 
     @Override
     public void onEnable() {
+        saveDefaultConfig();
+
+        /* UTILS */
         BukkitCommandHandler handler = BukkitCommandHandler.create(this);
+        db.connect();
 
         /* COMMANDS */
         handler.register(
@@ -26,6 +34,11 @@ public final class MAB extends JavaPlugin {
 
     @Override
     public void onDisable() {
+        db.close();
         System.out.println("Plugin disabled");
+    }
+
+    public Connection getConnection() throws SQLException {
+        return db.getConnection();
     }
 }
---
+++ src/main/java/gg/gyro/mab/utils/Database.java
@@ -0,0 +1,58 @@
+package gg.gyro.mab.utils;
+
+import gg.gyro.mab.MAB;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Objects;
+
+public class Database {
+    MAB plugin;
+    Connection connection;
+
+    public Database(MAB plugin) {
+        this.plugin = plugin;
+    }
+
+    public void connect() {
+        if (plugin.getConfig().getString("database.jdbc") == null) {
+            plugin.getLogger().severe("Please, add a database in the config.yml file!");
+            plugin.getServer().getPluginManager().disablePlugin(plugin);
+            return;
+        }
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            this.connection = DriverManager.getConnection(
+                    Objects.requireNonNull(this.plugin.getConfig().getString("database.jdbc")),
+                    this.plugin.getConfig().getString("database.user"),
+                    this.plugin.getConfig().getString("database.password"));
+        } catch (SQLException | ClassNotFoundException e) {
+            plugin.getLogger().severe("Error while connecting connection to Database");
+            plugin.getServer().getPluginManager().disablePlugin(plugin);
+        }
+    }
+
+    public void close() {
+        if (this.connection != null) {
+            try {
+                if (!this.connection.isClosed()) {
+                    this.connection.close();
+                }
+            } catch (SQLException e) {
+                plugin.getLogger().severe("Error while closing connection to Database");
+            }
+        }
+    }
+
+    public Connection getConnection() throws SQLException {
+        if (this.connection != null) {
+            if (!this.connection.isClosed()) {
+                return this.connection;
+            }
+        }
+        this.connect();
+        return this.connection;
+    }
+}
