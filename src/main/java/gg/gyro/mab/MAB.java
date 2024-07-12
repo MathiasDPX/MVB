91b4769597cb3eceba02158f05fb49fe1d0d795b
MathiasDPX <mathias@dupeux.net>
2024-07-12 20:57:55 +0200

    Use Lists

--- src/main/java/gg/gyro/mab/MAB.java
+++ src/main/java/gg/gyro/mab/MAB.java
@@ -2,16 +2,19 @@ package gg.gyro.mab;
 
 import gg.gyro.mab.commands.*;
 import gg.gyro.mab.listeners.*;
+
+import gg.gyro.mab.utils.Lists;
 import gg.gyro.mab.utils.Database;
 
 import revxrsal.commands.bukkit.BukkitCommandHandler;
 import org.bukkit.plugin.java.JavaPlugin;
+
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.Arrays;
 
 public final class MAB extends JavaPlugin {
     Database db = new Database(this);
+    Lists lists = new Lists();
 
     @Override
     public void onEnable() {
@@ -23,11 +26,11 @@ public final class MAB extends JavaPlugin {
         db.initialize();
 
         /* COMMANDS */
-        handler.getAutoCompleter().registerSuggestion("privacy_scope", (args, sender, command) -> {
-            return Arrays.asList("ping", "mp");
+        handler.getAutoCompleter().registerSuggestion("privacy_scopes", (args, sender, command) -> {
+            return lists.privacy_scopes;
         });
         handler.getAutoCompleter().registerSuggestion("gamemodes", (args, sender, command) -> {
-            return Arrays.asList("creative", "survival", "adventure", "spectator");
+            return lists.gamemodes;
         });
 
         handler.register(
@@ -55,5 +58,9 @@ public final class MAB extends JavaPlugin {
             e.printStackTrace();
         }
         return null;
+    }
+
+    public Lists getLists() {
+        return lists;
     }
 }
