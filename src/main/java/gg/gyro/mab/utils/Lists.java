5596de9000d4f2d8f9a3c0c6f79ab2933893b1f6
MathiasDPX <mathias@dupeux.net>
2024-07-12 20:58:08 +0200

    Create Lists

---
+++ src/main/java/gg/gyro/mab/utils/Lists.java
@@ -0,0 +1,9 @@
+package gg.gyro.mab.utils;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Lists {
+    public List<String> privacy_scopes = Arrays.asList("ping", "pm");
+    public List<String> gamemodes = Arrays.asList("creative", "survival", "adventure", "spectator");
+}
