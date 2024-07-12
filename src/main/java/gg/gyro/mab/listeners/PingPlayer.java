45886af993e7deedded84145bd33a1d0f07641e3
MathiasDPX <mathias@dupeux.net>
2024-07-13 00:56:53 +0200

    Add privacy

--- src/main/java/gg/gyro/mab/listeners/PingPlayer.java
+++ src/main/java/gg/gyro/mab/listeners/PingPlayer.java
@@ -1,5 +1,7 @@
 package gg.gyro.mab.listeners;
 
+import gg.gyro.mab.MAB;
+import gg.gyro.mab.utils.Privacy;
 import org.bukkit.Bukkit;
 import org.bukkit.Sound;
 import org.bukkit.event.EventHandler;
@@ -7,6 +9,14 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 public class PingPlayer implements Listener {
+    MAB plugin;
+    Privacy privacy;
+
+    public PingPlayer(MAB plugin) {
+        privacy = new Privacy(plugin);
+        this.plugin = plugin;
+    }
+
     @EventHandler
     public void onPlayerChat(AsyncPlayerChatEvent event) {
         String message = event.getMessage();
@@ -14,6 +24,7 @@ public class PingPlayer implements Listener {
         Bukkit.getOnlinePlayers().forEach(player -> {
             if (player.equals(event.getPlayer())) { return; }
             if (message.toLowerCase().contains(player.getName().toLowerCase())) {
+                if (!(privacy.getScope(player, "ping"))) { return; }
                 player.playSound(player.getEyeLocation(), Sound.BLOCK_NOTE_BLOCK_BELL, 1, 1);
             }
         });
