7e8e50411cb8499d47beb3ec5a397469b63008e7
MathiasDPX <mathias@dupeux.net>
2024-07-13 14:49:48 +0200

    Add response

--- src/main/java/gg/gyro/mab/utils/Lists.java
+++ src/main/java/gg/gyro/mab/utils/Lists.java
@@ -3,7 +3,7 @@ package gg.gyro.mab.utils;
 import java.util.Set;
 
 public class Lists {
-    public Set<String> privacy_scopes = Set.of("ping", "pm");
+    public Set<String> privacy_scopes = Set.of("ping", "pm", "response");
     public Set<String> gamemodes = Set.of("creative", "survival", "adventure", "spectator");
     public Set<String> config_parameters = Set.of("insomnia.enabled", "insomnia.chance");
 }
