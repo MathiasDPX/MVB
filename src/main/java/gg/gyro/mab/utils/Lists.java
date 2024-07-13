50511b6ef23cab954809b4a9fecc511d13a657f2
MathiasDPX <mathias@dupeux.net>
2024-07-13 12:04:01 +0200

    Add config parameters

--- src/main/java/gg/gyro/mab/utils/Lists.java
+++ src/main/java/gg/gyro/mab/utils/Lists.java
@@ -5,4 +5,5 @@ import java.util.Set;
 public class Lists {
     public Set<String> privacy_scopes = Set.of("ping", "pm");
     public Set<String> gamemodes = Set.of("creative", "survival", "adventure", "spectator");
+    public Set<String> config_parameters = Set.of("insomnia");
 }
