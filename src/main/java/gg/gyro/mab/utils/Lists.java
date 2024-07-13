16966ca1793c120d4bc6ea8a19056588ba2b7844
MathiasDPX <mathias@dupeux.net>
2024-07-14 00:50:51 +0200

    Use HashSet instead of Set

--- src/main/java/gg/gyro/mab/utils/Lists.java
+++ src/main/java/gg/gyro/mab/utils/Lists.java
@@ -1,10 +1,12 @@
 package gg.gyro.mab.utils;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
 
 public class Lists {
     public Set<String> privacy_scopes = Set.of("ping", "pm", "response");
     public Set<String> gamemodes = Set.of("creative", "survival", "adventure", "spectator");
-    public Set<String> config_parameters = Set.of("insomnia.enabled", "insomnia.chance");
-    public Set<String> utility_blocks = Set.of("anvil", "brewing_stand", "cartography_table", "crafting_table", "ender_chest", "fletching_table", "grindstone", "loom", "smithing_table", "stonecutter", "enchantment_table");
+    public HashSet<String> config_parameters = new HashSet<>(Arrays.asList("insomnia.enabled", "insomnia.chance"));
+    public HashSet<String> utility_blocks = new HashSet<>(Arrays.asList("anvil", "brewing_stand", "cartography_table", "crafting_table", "ender_chest", "fletching_table", "grindstone", "loom", "smithing_table", "stonecutter", "enchantment_table"));
 }
